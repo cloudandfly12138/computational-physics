3b89368532bb5a9e3953e69428c4da6eb96c2a01
Eli Lin <m18811692685@163.com>
2018-12-26 22:07:56 +0800

    Update WaveEquation_1D.m

--- HomeWork2/m/WaveEquation_1D.m
+++ HomeWork2/m/WaveEquation_1D.m
@@ -1,12 +1,15 @@
 %WaveEquation_1D
-%2018/12/4 ÁÖÏé
+%2018/12/4 æž—ç¥¥
 %y(i,l+1)=-y(i,l-1)+(2-2*lambda^2)*y(i,l)+lambda^2*(y(i+1,l)+y(i-1,l));
 clc; clear; format long;
-t0=0;  tn=1;  tau=1E-5; n=(tn-t0)/tau;
-x0=0; xn=1; h=0.01;  N=(xn-x0)/h;  x=x0:h:xn;
-v1=300; v2=150;
-lambda1=v1*tau/h;   lambda2=v2*tau/h;
-%³õÊ¼Ìõ¼þ ºÍ ±ß½çÌõ¼þ
+t0=0;  tn=1;  tau=1.25E-5; n=(tn-t0)/tau;
+x0=0;       xi=0.5;      xn=1; 
+h1=0.005;         h2=0.0025;    %xåœ¨(0,0.5)å’Œ(0.5,1)é‡‡ç”¨ä¸åŒæ­¥é•¿
+N1=(xi-x0)/h1;  N2=(xn-xi)/h2;     N=N1+N2;
+x1=x0:h1:xi;      x2=xi+h2:h2:xn;   x=[x1,x2];
+v1=300;             v2=150;
+lambda1=v1*tau/h1;   lambda2=v2*tau/h2;
+%åˆå§‹æ¡ä»¶ å’Œ è¾¹ç•Œæ¡ä»¶
 y=zeros(N+1,3);
 for i=1:N+1
     y(i,1)=exp(-1000*(x(i)-0.3)^2);
@@ -14,20 +17,20 @@ for i=1:N+1
 end
 y(1,:)=0;  
 
-%µü´úÇó½â
-for l=1:100 %Ê±¼äµü´ú
+%è¿­ä»£æ±‚è§£
+for l=1:800   %æ—¶é—´è¿­ä»£  æ±‚t=1E-3æ—¶ ä¸º80,1E-2æ—¶ä¸º800
     for i=2:N
-        if x(i)>0 && x(i)<0.5
+        if x(i)>0 && x(i)<xi    %lambda1==lambda2,å®žé™…ä¸ç”¨åˆ†æ”¯ç»“æž„å°±è¡Œ.
             y(i,3)=-y(i,1)+(2-2*lambda1^2)*y(i,2)+lambda1^2*(y(i+1,2)+y(i-1,2));
         else
             y(i,3)=-y(i,1)+(2-2*lambda2^2)*y(i,2)+lambda2^2*(y(i+1,2)+y(i-1,2));
         end
     end
-    y(N+1,3)=y(N,3);%±ß½çÌõ¼þ
+    y(N+1,3)=y(N,3);%è¾¹ç•Œæ¡ä»¶
     figure(2); set(gca,'Fontsize',16);
     plot(x,y(:,3));
     xlabel('x');ylabel('y'); grid on;
-    axis([0,1,-0.5,0.5]);
-    title(sprintf('t=%.5fÊ±¿Ì²¨ÐÎÍ¼',l*tau));
+    axis([0,1,-1,1]);
+    title(sprintf('t=%.5fæ—¶åˆ»æ³¢å½¢å›¾',l*tau));
     y(:,1)=y(:,2);  y(:,2)=y(:,3); 
 end
